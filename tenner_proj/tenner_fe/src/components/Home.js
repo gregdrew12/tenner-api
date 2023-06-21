d8226b06e8c0279e1cd4f8d06de56ce22ad7aedc
gregdrew12 <gregdrew12@gmail.com>
2023-06-20 23:58:48 -0400

    Home.js conversion from class to functional

--- tenner_proj/tenner_fe/src/components/Home.js
+++ tenner_proj/tenner_fe/src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import { Col, Container, Row } from "reactstrap";
 import UserList from "./UserList";
 import NewUserModal from "./NewUserModal";
@@ -7,42 +7,52 @@ import axios from "axios";
 
 import { API_URL } from "../constants";
 
-class Home extends Component {
-  state = {
+/*class Home extends Component*/
+function Home() {
+
+  /*state = {
     users: []
-  };
+  };*/
+  const [users, setUsers] = useState([]);
 
-  componentDidMount() {
+  /*componentDidMount() {
     this.resetState();
-  }
+  }*/
+  useEffect(() => {
+    this.resetState()
+  });
 
-  getUsers = () => {
+  /*getUsers = () => {
     axios.get(API_URL).then(res => this.setState({ users: res.data }));
+  };*/
+  const getUsers = () => {
+    axios.get(API_URL).then(res => setUsers(res.data));
   };
 
-  resetState = () => {
+  /*resetState = () => {
+    this.getUsers();
+  };*/
+  const resetState = () => {
     this.getUsers();
   };
 
-  render() {
-    return (
-      <Container style={{ marginTop: "20px" }}>
-        <Row>
-          <Col>
-            <UserList
-              users={this.state.users}
-              resetState={this.resetState}
-            />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <NewUserModal create={true} resetState={this.resetState} />
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+  return (
+    <Container style={{ marginTop: "20px" }}>
+      <Row>
+        <Col>
+          <UserList
+            users={users}
+            resetState={resetState}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <NewUserModal create={true} resetState={resetState} />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Home;

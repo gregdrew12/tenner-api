7e369390c656e854f17177659dbb357b14c4b971
gregdrew12 <gregdrew12@gmail.com>
2023-06-20 21:54:01 -0400

    backend auth

--- tenner_proj/backend/users/views.py
+++ tenner_proj/backend/users/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, APIView
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from .models import User
 from .serializers import *
@@ -48,3 +49,14 @@ class HomeView(APIView):
     def get(self, request):
         content = {'message': 'Welcome to the JWT Authentication page using React Js and Django!'}
         return Response(content)
+    
+class LogoutView(APIView):
+     permission_classes = (IsAuthenticated,)
+     def post(self, request):
+          try:
+               refresh_token = request.data["refresh_token"]
+               token = RefreshToken(refresh_token)
+               token.blacklist()
+               return Response(status=status.HTTP_205_RESET_CONTENT)
+          except Exception as e:
+               return Response(status=status.HTTP_400_BAD_REQUEST)

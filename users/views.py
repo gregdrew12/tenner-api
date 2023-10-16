85a88ec74aaf224ccc896b961eebf29ac5fec244
gregdrew12 <gregdrew12@gmail.com>
2023-10-16 18:52:56 -0400

    Add console prints for interceptor debugging

--- users/views.py
+++ users/views.py
@@ -52,11 +52,13 @@ class HomeView(APIView):
 class LogoutView(APIView):
     permission_classes = (IsAuthenticated, )
     def post(self, request):
+        print(request.META.get('HTTP_AUTHORIZATION', ''))
         try:
             refresh_token = request.data["refresh_token"]
             token = RefreshToken(refresh_token)
             token.blacklist()
             return Response(status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
+            print(e)
             return Response(status=status.HTTP_400_BAD_REQUEST)
           
